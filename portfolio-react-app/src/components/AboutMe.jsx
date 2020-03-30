6d798d7fd4f35c44e1f04c8eecbe6b11f1d377ce
MaryHooker <mc020791@yahoo.com>
2020-03-30 12:00:40 -0500

    about me basic

--- portfolio-react-app/src/components/AboutMe.jsx
+++ portfolio-react-app/src/components/AboutMe.jsx
@@ -1,17 +1,36 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import Resume from './Resume';
+import {BrowserRouter as Router,Link,Route} from 'react-router-dom';
+// import Image from '/home/student/Downloads/image0 (5).jpeg';
 
 class AboutMe extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div>
-                <h2>About Me</h2>
+    render() {
+        return (
+            <div className='aboutMeContainer'>
+                <div>
+                    <img src='https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX38348134.jpg' alt="avatar" className='image' />
+                </div>
+                <div className='aboutMeText'>
+                    <p>Hi! My name is Mary Hooker and I'm a full stack software developer.  I'm most interested in front-end web development but I'm open to all positions and push for the best in everything I do. My education started with CodeCrew Code School and I intend to further my education throughout my career.</p>
+                </div>
+                <div className='logo'>
+                    <img src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/4971/s300/code-school-logo-3-3-.png" alt="logo"/>
+                </div>
+                <div className='resume'>
+                    <Router>
+                        <Link to='/resume'>Resume</Link>
+                        <Route path='/resume'>
+                            <Resume/>
+                        </Route>
+                    </Router>
+                </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default AboutMe;
---
+++ portfolio-react-app/src/components/Resume.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class Resume extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1>Resume</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Resume;
