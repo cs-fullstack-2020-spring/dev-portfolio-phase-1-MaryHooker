c8c6a4f3449dc826dcebfdc8f0c66e3220965100
MaryHooker <mc020791@yahoo.com>
2020-03-30 14:28:39 -0500

    skill logos

--- portfolio-react-app/src/components/Projects.jsx
+++ portfolio-react-app/src/components/Projects.jsx
@@ -7,8 +7,33 @@ class Projects extends Component {
     }
     render() { 
         return ( 
-            <div>
-                <h2>Projects</h2>
+            <div className='projectsContainer'>
+
+                <div className='languageLogos'>
+
+                    <h3>Skills</h3>
+               
+                    <img src="https://i7.pngguru.com/preview/840/443/663/web-development-html-css3-canvas-element-web-design-w3c-html5-logo.jpg" alt="html" className='htmlLogo'/>
+                
+                    <img src="https://p1.hiclipart.com/preview/326/868/1022/css3-badge-blue-and-white-css-icon-png-clipart.jpg" alt="css" className='cssLogo'/>
+               
+              
+                    <img src="https://thumbnail.imgbin.com/0/8/11/imgbin-javascript-logo-html-javascript-logo-1gJt6c05WzKXQuybWYgXvaTYk_t.jpg" alt="js" className='jsLogo'/>
+                
+                
+                    <img src="https://toppng.com/uploads/preview/react-native-svg-transformer-allows-you-import-svg-aperture-science-innovators-logo-11562851994zqcpwozsvy.png" alt="react" className='reactLogo'/>
+                
+                </div>
+
+                <div>
+
+                    <h3>Projects</h3>
+
+                    
+                </div>
+
+                
+
             </div>
          );
     }
