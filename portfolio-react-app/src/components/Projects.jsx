c38ccfefc7d463227aa6ffab8289588fa94408a7
MaryHooker <mc020791@yahoo.com>
2020-03-30 14:58:45 -0500

    projects layout

--- portfolio-react-app/src/components/Projects.jsx
+++ portfolio-react-app/src/components/Projects.jsx
@@ -1,42 +1,46 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 class Projects extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className='projectsContainer'>
 
-                <div className='languageLogos'>
-
-                    <h3>Skills</h3>
-               
-                    <img src="https://i7.pngguru.com/preview/840/443/663/web-development-html-css3-canvas-element-web-design-w3c-html5-logo.jpg" alt="html" className='htmlLogo'/>
-                
-                    <img src="https://p1.hiclipart.com/preview/326/868/1022/css3-badge-blue-and-white-css-icon-png-clipart.jpg" alt="css" className='cssLogo'/>
-               
-              
-                    <img src="https://thumbnail.imgbin.com/0/8/11/imgbin-javascript-logo-html-javascript-logo-1gJt6c05WzKXQuybWYgXvaTYk_t.jpg" alt="js" className='jsLogo'/>
-                
-                
-                    <img src="https://toppng.com/uploads/preview/react-native-svg-transformer-allows-you-import-svg-aperture-science-innovators-logo-11562851994zqcpwozsvy.png" alt="react" className='reactLogo'/>
-                
+                <div className='html'>
+                    <img src="https://i7.pngguru.com/preview/840/443/663/web-development-html-css3-canvas-element-web-design-w3c-html5-logo.jpg" alt="html" className='htmlLogo' />
+                </div>
+                <div className='css'>
+                    <img src="https://p1.hiclipart.com/preview/326/868/1022/css3-badge-blue-and-white-css-icon-png-clipart.jpg" alt="css" className='cssLogo' />
+                </div>
+                <div className='js'>
+                    <img src="https://thumbnail.imgbin.com/0/8/11/imgbin-javascript-logo-html-javascript-logo-1gJt6c05WzKXQuybWYgXvaTYk_t.jpg" alt="js" className='jsLogo' />
+                </div>
+                <div className='react'>
+                    <img src="https://toppng.com/uploads/preview/react-native-svg-transformer-allows-you-import-svg-aperture-science-innovators-logo-11562851994zqcpwozsvy.png" alt="react" className='reactLogo' />
                 </div>
 
-                <div>
-
-                    <h3>Projects</h3>
-
-                    
+                <div className='project1'>
+                    <h3>Project 1</h3>
+                    <h4>Image/Link</h4>
+                    <p>Description of Project 1</p>
+                </div>
+                <div className='project2'>
+                    <h3>Project 2</h3>
+                    <h4>Image/Link</h4>
+                    <p>Description of Project 2</p>
+                </div>
+                <div className='project3'>
+                    <h3>Project 3</h3>
+                    <h4>Image/Link</h4>
+                    <p>Description of Project 3</p>
                 </div>
 
-                
-
-            </div>
-         );
+            </div >
+        );
     }
 }
- 
+
 export default Projects;
