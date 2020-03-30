565e62d38869b636eea8aa0dd9e2098ca1a643f3
MaryHooker <mc020791@yahoo.com>
2020-03-30 11:28:02 -0500

    links made

--- portfolio-react-app/src/components/OpeningPage.jsx
+++ portfolio-react-app/src/components/OpeningPage.jsx
@@ -1,23 +1,36 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Contact from './Contact';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 class OpeningPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
+                <Router>
+                    <Link to="/">Home</Link>
+                    <Link to="/aboutMe">About Me</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Route path="/aboutMe">
+                        <AboutMe />
+                    </Route>
+                    <Route path="/projects">
+                        <Projects />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                </Router>
                 <h1>Welcome</h1>
-                <AboutMe/>
-                <Projects/>
-                <Contact/>
             </div>
-         );
+        );
     }
 }
- 
+
 export default OpeningPage;
