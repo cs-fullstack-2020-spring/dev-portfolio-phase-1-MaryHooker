9122a67fb6a020a3195297c1639e0a2c6bf70e6e
MaryHooker <mc020791@yahoo.com>
2020-04-02 15:23:25 -0500

    react animated css on opening title

--- portfolio-react-app/src/components/OpeningPage.jsx
+++ portfolio-react-app/src/components/OpeningPage.jsx
@@ -4,32 +4,33 @@ import Projects from './Projects';
 import Contact from './Contact';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Resume from './Resume';
+import { Animated } from "react-animated-css";
 // import CSSTransitionGroup from 'react-transition-group';
 
 class OpeningPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+
         }
     }
 
-    hideWelcome = (event) =>{
-        let tagDisplay= document.querySelector('#display')
+    hideWelcome = (event) => {
+        let tagDisplay = document.querySelector('#display')
         //sanity
         console.log(tagDisplay.innerText)
 
-    
-          if(event.target.name==='aboutMe'){
-            tagDisplay.innerText='';
-        } else if(event.target.name==='projects'){
-            tagDisplay.innerText='';
-        } else if(event.target.name==='contact'){
-            tagDisplay.innerText='';
-        } else if(event.target.name==='resume'){
-            tagDisplay.innerText='';
-        } else if(event.target.name==='homeL'){
-            tagDisplay.innerText='Welcome';
+
+        if (event.target.name === 'aboutMe') {
+            tagDisplay.innerText = '';
+        } else if (event.target.name === 'projects') {
+            tagDisplay.innerText = '';
+        } else if (event.target.name === 'contact') {
+            tagDisplay.innerText = '';
+        } else if (event.target.name === 'resume') {
+            tagDisplay.innerText = '';
+        } else if (event.target.name === 'homeL') {
+            tagDisplay.innerText = 'Welcome';
         }
     }
 
@@ -38,20 +39,20 @@ class OpeningPage extends Component {
             <div className='links'>
                 {/* <CSSTransitionGroup transitionName='group'> */}
                 <Router>
-                    
-                        <Link to="/" name='homeL' className='homeL' onClick={this.hideWelcome}>Home</Link>
-                    
-                    
-                        <Link to="/aboutMe" name='aboutMe' className='aboutMeL' onClick={this.hideWelcome}>About Me</Link>
-                    
-                   
-                        <Link to="/projects" name='projects' className='projectsL' onClick={this.hideWelcome}>Projects</Link>
-                   
-                    
-                        <Link to="/contact" name='contact' className='contactL' onClick={this.hideWelcome}>Contact</Link>
 
-                        <Link to="/resume" name='resume' className='resumeL' onClick={this.hideWelcome}>Resume</Link>
-                    
+                    <Link to="/" name='homeL' className='homeL' onClick={this.hideWelcome}>Home</Link>
+
+
+                    <Link to="/aboutMe" name='aboutMe' className='aboutMeL' onClick={this.hideWelcome}>About Me</Link>
+
+
+                    <Link to="/projects" name='projects' className='projectsL' onClick={this.hideWelcome}>Projects</Link>
+
+
+                    <Link to="/contact" name='contact' className='contactL' onClick={this.hideWelcome}>Contact</Link>
+
+                    <Link to="/resume" name='resume' className='resumeL' onClick={this.hideWelcome}>Resume</Link>
+
                     <Route path="/aboutMe">
                         <AboutMe />
                     </Route>
@@ -59,17 +60,20 @@ class OpeningPage extends Component {
                         <Projects />
                     </Route>
                     <Route path="/contact">
-                        <Contact/>
+                        <Contact />
                     </Route>
                     <Route path="/resume">
-                        <Resume/>
+                        <Resume />
                     </Route>
                 </Router>
                 {/* </CSSTransitionGroup> */}
-                <div>
-                    {this.tagDisplay}
-                    <h1 id='display' className='welcome'>Welcome</h1>
-                </div>
+                <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+                    <div>
+                        {this.tagDisplay}
+                        <h1 id='display' className='welcome'>Welcome</h1>
+                    </div>
+                </Animated>
+
             </div>
         );
     }
