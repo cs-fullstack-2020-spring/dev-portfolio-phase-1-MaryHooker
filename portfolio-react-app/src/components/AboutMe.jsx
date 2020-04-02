b7e58bdb9e9bfe2a3b2b7109725806380d59a3a4
MaryHooker <mc020791@yahoo.com>
2020-04-02 15:36:20 -0500

    css animations

--- portfolio-react-app/src/components/AboutMe.jsx
+++ portfolio-react-app/src/components/AboutMe.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 // import Resume from './Resume';
 // import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 // import OpeningPage from './OpeningPage';
+import { Animated } from "react-animated-css";
+
 
 class AboutMe extends Component {
     constructor(props) {
@@ -15,9 +17,11 @@ class AboutMe extends Component {
         return (
             <div className='aboutMeContainer'>
 
-                <div className='imageMe'>
-                    <img src="/me3.gif" alt="me" className='imageSize' />
-                </div>
+                <Animated animationIn="slideInDown" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
+                    <div className='imageMe'>
+                        <img src="/me3.gif" alt="me" className='imageSize' />
+                    </div>
+                </Animated>
 
                 <div className='aboutMeText'>
                     <p>Hi! My name is Mary Hooker and I'm a full stack software developer.  I'm most interested in front-end web development but I'm open to all positions and push for the best in everything I do. My education started with CodeCrew Code School and I intend to further my education throughout my career.</p>
--- portfolio-react-app/src/components/Projects.jsx
+++ portfolio-react-app/src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Animated } from "react-animated-css";
 
 class Projects extends Component {
     constructor(props) {
@@ -7,44 +8,54 @@ class Projects extends Component {
     }
     render() {
         return (
-            <div className='projectsContainer'>
+            <div>
+                <Animated animationIn="bounceIn" animationOut="rotateOutDownRight" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
 
-                <div className='html'>
-                    <img src="/html5.png" alt="html" className='htmlLogo' />
-                </div>
-                <div className='css'>
-                    <img src="/css.png" alt="css" className='cssLogo' />
-                </div>
-                <div className='js'>
-                    <img src="/js.png" alt="js" className='jsLogo' />
-                </div>
-                <div className='react'>
-                    <img src="/react2.png" alt="react" className='reactLogo' />
-                </div>
+                <div className='projectsContainer'>
 
-                <div className='project1'>
-                    <h4 className='dreamTitle'>Dream Journal App</h4>
-                    <a href="https://github.com/cs-fullstack-2020-spring/200317-react-forms-events-practice-MaryHooker" className='dreamProjectLink'>Check It Out</a>
-                    <img src="./dream2.png" alt="dream joural" className='dreamImage'/>
-                    <br/>
-                    <p className='dreamDescription'>This react application gives the user a chance to record everything they would need to remember about their dreams using a form</p>
-                </div>
-                <div className='project2'>
-                    <h4 className='bold'>Zodiac App</h4>
-                    <a href="https://github.com/MaryHooker/Projects/tree/master/zodiac-app" className='projectLinks'>Check It Out</a>
-                    <img src="./zodiac2.png" alt="zodiacImage" className='zodiacImage'/>
-                    <br/>
-                    <p className='zodiacDescription'>This react application provides the user with selective information about their own zodiac sign using images as links</p>
-                </div>
-                <div className='project3'>
-                    <h4 className='bold'>Favorites App</h4>
-                    <a href="https://github.com/MaryHooker/Projects/tree/master/favorite-things-app" className='projectLinks'>Check It Out</a>
-                    <img src="./favorites2.png" alt="favoritesImage" className='favoritesImage'/>
-                    <br/>        
-                    <p className='favoritesDescription'>This react application tells the user of my own personal favorites in my chosen categories and asks for the users as well using links</p>
-                   
-                </div>
+                    <div className='html'>
+                        <img src="/html5.png" alt="html" className='htmlLogo' />
+                    </div>
+                    <div className='css'>
+                        <img src="/css.png" alt="css" className='cssLogo' />
+                    </div>
+                    <div className='js'>
+                        <img src="/js.png" alt="js" className='jsLogo' />
+                    </div>
+                    <div className='react'>
+                        <img src="/react2.png" alt="react" className='reactLogo' />
+                    </div>
 
+
+
+                    <div className='project1'>
+                        <h4 className='dreamTitle'>Dream Journal App</h4>
+                        <a href="https://github.com/cs-fullstack-2020-spring/200317-react-forms-events-practice-MaryHooker" className='dreamProjectLink'>Check It Out</a>
+                        <img src="./dream2.png" alt="dream joural" className='dreamImage' />
+                        <br />
+                        <p className='dreamDescription'>This react application gives the user a chance to record everything they would need to remember about their dreams using a form</p>
+                    </div>
+
+                    <div className='project2'>
+                        <h4 className='bold'>Zodiac App</h4>
+                        <a href="https://github.com/MaryHooker/Projects/tree/master/zodiac-app" className='projectLinks'>Check It Out</a>
+                        <img src="./zodiac2.png" alt="zodiacImage" className='zodiacImage' />
+                        <br />
+                        <p className='zodiacDescription'>This react application provides the user with selective information about their own zodiac sign using images as links</p>
+                    </div>
+
+                    <div className='project3'>
+                        <h4 className='bold'>Favorites App</h4>
+                        <a href="https://github.com/MaryHooker/Projects/tree/master/favorite-things-app" className='projectLinks'>Check It Out</a>
+                        <img src="./favorites2.png" alt="favoritesImage" className='favoritesImage' />
+                        <br />
+                        <p className='favoritesDescription'>This react application tells the user of my own personal favorites in my chosen categories and asks for the users as well using links</p>
+
+                    </div>
+
+
+                </div >
+                </Animated>
             </div >
         );
     }
