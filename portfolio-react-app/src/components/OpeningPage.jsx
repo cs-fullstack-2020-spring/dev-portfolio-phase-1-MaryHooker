d8a1cb5c6022fd010424b5ed2b18bb2074a320df
MaryHooker <mc020791@yahoo.com>
2020-04-03 12:58:12 -0500

    nav link/reformatted each page to fit new nav bar/styling

--- portfolio-react-app/src/components/OpeningPage.jsx
+++ portfolio-react-app/src/components/OpeningPage.jsx
@@ -36,37 +36,92 @@ class OpeningPage extends Component {
     render() {
         return (
             <div className='links'>
-                {/* <CSSTransitionGroup transitionName='group'> */}
-                <Router>
 
-                    <Link to="/" name='homeL' className='homeL' onClick={this.hideWelcome}>Home</Link>
+                <Animated animationIn="bounceInLeft" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
+                    
+                    <Router>
+                        {/* Bootstrap Navigation Bar */}
+                        <ul className="nav">
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/" name='homeL' onClick={this.hideWelcome}>
+                                    Home
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/aboutMe" name='aboutMe' onClick={this.hideWelcome}>
+                                    About Me
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/projects" name='projects' onClick={this.hideWelcome}>
+                                    Projects
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact" name='contact' onClick={this.hideWelcome}>
+                                    Contact
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/resume" name='resume' onClick={this.hideWelcome}>
+                                    Resume
+                                </Link>
+                            </li>
+                        </ul>
+                        {/* Routes for Simple Grid and React Cards example */}
+                        <Route path="/aboutMe">
+                            <AboutMe />
+                        </Route>
+
+                        <Route path="/projects">
+                            <Projects />
+                        </Route>
+
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+
+                        <Route path="/resume">
+                            <Resume />
+                        </Route>
+
+                    </Router>
+
+                    {/* <Router>
+
+                        <Link to="/" name='homeL' className='homeL' onClick={this.hideWelcome}>Home</Link>
 
 
-                    <Link to="/aboutMe" name='aboutMe' className='aboutMeL' onClick={this.hideWelcome}>About Me</Link>
+                        <Link to="/aboutMe" name='aboutMe' className='aboutMeL' onClick={this.hideWelcome}>About Me</Link>
 
 
-                    <Link to="/projects" name='projects' className='projectsL' onClick={this.hideWelcome}>Projects</Link>
+                        <Link to="/projects" name='projects' className='projectsL' onClick={this.hideWelcome}>Projects</Link>
 
 
-                    <Link to="/contact" name='contact' className='contactL' onClick={this.hideWelcome}>Contact</Link>
+                        <Link to="/contact" name='contact' className='contactL' onClick={this.hideWelcome}>Contact</Link>
 
-                    <Link to="/resume" name='resume' className='resumeL' onClick={this.hideWelcome}>Resume</Link>
+                        <Link to="/resume" name='resume' className='resumeL' onClick={this.hideWelcome}>Resume</Link>
 
-                    <Route path="/aboutMe">
-                        <AboutMe />
-                    </Route>
-                    <Route path="/projects">
-                        <Projects />
-                    </Route>
-                    <Route path="/contact">
-                        <Contact />
-                    </Route>
-                    <Route path="/resume">
-                        <Resume/>
-                    </Route>
-                </Router>
-                {/* </CSSTransitionGroup> */}
-                <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+                        <Route path="/aboutMe">
+                            <AboutMe />
+                        </Route>
+                        <Route path="/projects">
+                            <Projects />
+                        </Route>
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route path="/resume">
+                            <Resume />
+                        </Route>
+                    </Router> */}
+                    
+
                     <div>
                         {this.tagDisplay}
                         <h1 id='display' className='welcome'>Welcome</h1>
